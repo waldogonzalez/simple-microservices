77ac06279c73090c151502a9e22bebd549d4de17
Marcelo Robles <mroblesa.21@gmail.com>
2023-07-22 01:18:33 -0400

    DSPIK-67 (FEAT) Add cors

--- apigateway/src/main/java/com/krah/spik/config/SecurityConfiguration.java
+++ apigateway/src/main/java/com/krah/spik/config/SecurityConfiguration.java
@@ -1,17 +1,29 @@
 package com.krah.spik.config;
 
+import jakarta.ws.rs.HttpMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebFluxSecurity()
 public class SecurityConfiguration {
 
+    private final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
     @Value("${auth0.audience}")
     private String audience;
 
@@ -19,13 +31,20 @@ public class SecurityConfiguration {
     private String issuer;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.oauth2Login();
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        log.info("entra security");
+        http.cors().configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of("*"));
+                    configuration.setAllowedMethods(List.of("*"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    return configuration;
+                }).and().csrf().disable().oauth2ResourceServer().jwt();
         return http.build();
     }
 
     @Bean
-    JwtDecoder jwtDecoder() {
+    public JwtDecoder jwtDecoder() {
         NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
 
         OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
